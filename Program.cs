dab51998269af25ad77051783f5f9ec56504739a
Austin Rogers <austinrogers@Austins-MacBook-Pro-2.local>
2024-09-08 21:20:36 -0500

    Final

--- Program.cs
+++ Program.cs
@@ -1,19 +1,15 @@
 ﻿//Variables
-
-using System.Runtime.ConstrainedExecution;
-using System.Security.AccessControl;
-
 double numberOfSandwisches = 0;
 double numberOfToppings = 0;
 double tip = 0;
 
 //Fixed Cost 
-double COST_OF_SANDWICH = 4.75;
-double Cost_OF_TOPPING = 0.55;
-double DISCOUNT_AMOUNT = 0.10;
+const double COST_OF_SANDWICH = 4.75;
+const double COST_OF_TOPPING = 0.55;
+const double DISCOUNT_AMOUNT = 0.10;
 
 //get sandwhich number
-System.Console.WriteLine("How many sandwiches are you buying");
+System.Console.WriteLine("How many sandwiches are you buying?");
 numberOfSandwisches = double.Parse(Console.ReadLine());
 
 //get topping number 
@@ -26,7 +22,7 @@ tip = double.Parse(Console.ReadLine());
 
 //math for order cost
 double totalSandwichCost = COST_OF_SANDWICH * numberOfSandwisches;
-double totalToppingCost = Cost_OF_TOPPING * numberOfToppings;
+double totalToppingCost = COST_OF_TOPPING * numberOfToppings;
 double baseCost = totalSandwichCost + totalToppingCost;
 double orderCost= tip + baseCost * (1-DISCOUNT_AMOUNT);
 
--- obj/Debug/net8.0/mis-221-lab-2-arog67.AssemblyInfo.cs
+++ obj/Debug/net8.0/mis-221-lab-2-arog67.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mis-221-lab-2-arog67")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6beb5fcdfcf434119a0b4b5205353d107cac576")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c254ea210140c2610fe87c5bc8e9c66dd94fbcb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("mis-221-lab-2-arog67")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mis-221-lab-2-arog67")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
